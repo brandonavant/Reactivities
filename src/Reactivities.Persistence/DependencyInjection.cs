8f4ed30509d42dca29b2acb77f3423bcdd0288b1
Brandon Avant <avant.brandon@gmail.com>
2020-10-07 09:47:02 -0500

    Initial commit

---
+++ src/Reactivities.Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Reactivities.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
